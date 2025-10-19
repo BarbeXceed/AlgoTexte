d3212190756127005aaafaaa41d4431c75e5c107
BarbeXceed <barberiepbe@gmail.com>
2025-10-19 18:50:20 +0200

    Modif du main por passer le pattern en argument ainsi que le test de MorriPrat

--- TP_Window/main.cpp
+++ TP_Window/main.cpp
@@ -1,16 +1,24 @@
 #include "naif.hpp"
+#include "MorrisPrat.hpp"
 #include "lectureFichier.hpp"
+#include "affichageVector.hpp"
 
 int main(int argc, char* argv[]){
-    if(argc != 2){
+    if(argc < 3){
         cout << "Erreur : Mongolo" << endl;
         exit(EXIT_FAILURE);
     } else{
 
         string mot = lectureFichier(argv[1]);
-        string pattern = "the";
+        string pattern = argv[2];
         cout << AlgoNaif(mot, pattern) << endl;
-        
+
+        /*
+        vector<int> tab = tableBords(pattern);
+        coutVector(tab);
+        */
+
+        cout << morrisPrat(mot, pattern) << endl;
         return EXIT_SUCCESS;
     }
 }
